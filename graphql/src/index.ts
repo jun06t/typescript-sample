4ea417555e638ac778da039ca7e8f6d62346ff0c
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2023-12-05 10:27:41 +0900

    feat: use dataloader

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { PrismaClient } from "@prisma/client";
+import { Post, PrismaClient } from "@prisma/client";
+import DataLoader from "dataloader";
 const prisma = new PrismaClient();
 
 const typeDefs = `#graphql
@@ -19,12 +20,28 @@ const typeDefs = `#graphql
   }
 `;
 
+type BatchPost = (userIds: readonly number[]) => Promise<Post[][]>;
+
+// DataLoader function to batch and cache post requests
+const batchPosts: BatchPost = async (userIds) => {
+  const posts = await prisma.post.findMany({
+    where: {
+      authorId: { in: [...userIds] },
+    },
+  });
+  const postsByUserId = userIds.map((userId: number) =>
+    posts.filter((post) => post.authorId === userId)
+  );
+  return postsByUserId;
+};
+
+// Create a DataLoader instance for posts
+const postsLoader = new DataLoader<number, Post[]>(batchPosts);
+
 const resolvers = {
   User: {
     posts: async (parent: any) => {
-      return await prisma.post.findMany({
-        where: { authorId: parent.id },
-      });
+      return await postsLoader.load(parent.id);
     },
   },
   Query: {
